b59840dd4d094e16e058c26e3df8eee3b531ddd8
Adedoyin <isradeoluwa@gmail.com>
2023-05-18 16:52:25 +0100

    Expense Item: Hardcoded values

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import "./ExpenseItem.css"
+
+export default function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>May 18, 2023</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
+}
