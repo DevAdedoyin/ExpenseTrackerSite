05ce0738f6afdcd4255bcd743b2b0ea437a14c8c
Adedoyin <isradeoluwa@gmail.com>
2023-05-20 22:30:44 +0100

    Expense Item: Card created

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,8 +1,8 @@
 import "./ExpenseDate.css";
 
-export default function ExpenseDate(props) {
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+function ExpenseDate(props) {
   const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
 
   return (
@@ -13,3 +13,5 @@ export default function ExpenseDate(props) {
     </div>
   );
 }
+
+export default ExpenseDate;
